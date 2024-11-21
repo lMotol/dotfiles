8ded3055d782ce24d462e22b90b356be1eefcd10
motomasa.suzuki <m_suzuki@suzukitsuyoshirinoMacBook-Pro.local>
2024-11-21 16:36:00 +0900

    feat: tmux plugin manager, nvim のインストールと設定ファイルのコピーの改善

--- install.sh
+++ install.sh
@@ -1,12 +1,50 @@
 #!/bin/bash -e
 
-sudo apt-get install curl git ripgrep neovim tmux
+# 必要なツールをインストール
+sudo apt-get install -y curl git ripgrep tmux
 
+# tmux plugin manager のインストール
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+~/.tmux/plugins/tpm/bin/install_plugins
+
+# nvim のインストール
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage --appimage-extract
+./squashfs-root/AppRun --version
+sudo mv squashfs-root /
+sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+which nvim
+nvim --version
+
+# 無視するパターンを定義
 IGNORE_PATTERN="^\.(git|travis)"
 
-echo "Create dotfile links."
+# シンボリックリンクを作成する関数
+link_dotfiles() {
+    local src=$1
+    local dest=$2
+
+    # ディレクトリの場合、再帰的にリンクを作成
+    if [[ -d $src ]]; then
+        mkdir -p "$dest" # 必要ならディレクトリを作成
+        for file in "$src"/*; do
+            link_dotfiles "$file" "$dest/$(basename "$file")"
+        done
+    else
+        ln -snfv "$src" "$dest"
+    fi
+}
+
+echo "Creating dotfile links..."
+
+# カレントディレクトリ内のドットファイルを処理
 for dotfile in .??*; do
-	[[ $dotfile =~ $IGNORE_PATTERN ]] && continue
-	ln -snfv "$(pwd)/$dotfile" "$HOME/$dotfile"
+    # 無視するファイルをスキップ
+    [[ $dotfile =~ $IGNORE_PATTERN ]] && continue
+
+    # ディレクトリ構造を維持しながらリンクを作成
+    link_dotfiles "$(pwd)/$dotfile" "$HOME/$dotfile"
 done
+
 echo "Success"
