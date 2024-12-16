f9fa9325f4244743dde5405b496fc6bb9e24e893
lMotol <motomasa_s@sslab.ics.keio.ac.jp>
2024-12-16 06:11:12 +0000

    fix: install.sh の修正

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 
-# 必要なツールをインストール
 sudo apt-get install -y curl git ripgrep build-essential tmux fd-find unzip
 
 # tmux plugin manager のインストール
@@ -40,27 +39,17 @@ else
 fi
 
 # npm のインストール
-# npm がインストールされているか確認
 if command -v npm &>/dev/null; then
     echo "npm is already installed (version: $(npm -v))"
 else
     echo "npm is not installed. Installing..."
-
-    # nvm がインストールされているか確認
     if command -v nvm &>/dev/null; then
         echo "Using nvm to install Node.js and npm..."
         nvm install --lts
     else
-        # nvm がインストールされていない場合、nvm をインストール
         echo "nvm is not installed. Installing nvm..."
-
-        # nvm インストールスクリプトを実行
         curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-
-        # nvm のインストールが完了した後、シェルを再読み込み
         echo "nvm has been installed. Please restart your terminal or run 'source ~/.bashrc' (or source ~/.zshrc if you use zsh)."
-
-        # インストール後、nvm を使って Node.js と npm をインストール
         echo "Using nvm to install Node.js and npm..."
         export NVM_DIR="$HOME/.nvm"
         [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
@@ -71,16 +60,12 @@ else
     npm -v
 fi
 
-# 無視するパターンを定義
+# dotfiles のシンボリックリンクを作成
 IGNORE_PATTERN="^\.(git|travis)"
 
-# シンボリックリンクを作成する関数
 link_dotfiles() {
     local src=$1
     local dest=$2
-
-    # ディレクトリの場合、再帰的にリンクを作成
-    if [[ -d $src ]]; then
         mkdir -p "$dest" # 必要ならディレクトリを作成
         for file in "$src"/*; do
             link_dotfiles "$file" "$dest/$(basename "$file")"
@@ -92,26 +77,22 @@ link_dotfiles() {
 
 echo "Creating dotfile links..."
 
-# カレントディレクトリ内のドットファイルを処理
-# for dotfile in .??*; do
 cd ~/dotfiles
 find . -maxdepth 1 -name ".*" ! -name "." ! -name ".." | while read -r dotfile; do
-    # 無視するファイルをスキップ
     basename_dotfile=$(basename "$dotfile")
     [[ $basename_dotfile =~ $IGNORE_PATTERN ]] && continue
 
     echo "$dotfile"
-
-    # ディレクトリ構造を維持しながらリンクを作成
     if [[ $dotfile == ".config" ]]; then
         echo "dsvdssdvf"
         for subdir in "$dotfile"/*; do
             link_dotfiles "$(pwd)/$subdir" "$HOME/$dotfile/$(basename "$subdir")"
         done
     else
-        # 通常のドットファイルやディレクトリはそのままリンク
         link_dotfiles "$(pwd)/$dotfile" "$HOME/$dotfile"
     fi
 done
 
 echo "Success"
+echo "シェルの設定ファイルが変更された場合はシェルの再読み込みを行うこと"
+echo "tmux の設定を読み込むために tmux を起動し prefox + I を入力すること"
