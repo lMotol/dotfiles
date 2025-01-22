cf904aa1d969d04a745c29978cb38b4c5fc169b7
motomasa.suzuki <m_suzuki@suzukitsuyoshirinoMacBook-Pro.local>
2025-01-22 21:55:04 +0900

    fix: install.sh の fzf の問題の対策

--- install.sh
+++ install.sh
@@ -2,6 +2,22 @@
 
 sudo apt-get install -y curl git ripgrep build-essential tmux fd-find unzip
 
+# fzf のインストール
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+CURRENT_SHELL=$(basename "$SHELL")
+if [ "$CURRENT_SHELL" = "bash" ]; then
+    CONFIG_FILE="$HOME/.bashrc"
+    ADD_LINE='eval "$(fzf --bash)"'
+elif [ "$CURRENT_SHELL" = "zsh" ]; then
+    CONFIG_FILE="$HOME/.zshrc"
+    ADD_LINE='source <(fzf --zsh)'
+else
+    echo "サポートされていないシェルです: $CURRENT_SHELL"
+    exit 1
+fi
+grep -qxF "$ADD_LINE" "$CONFIG_FILE" || echo "$ADD_LINE" >>"$CONFIG_FILE"
+
 # tmux plugin manager のインストール
 TARGET_DIR="$HOME/.tmux/plugins/tpm"
 if [ -d "$TARGET_DIR" ]; then
