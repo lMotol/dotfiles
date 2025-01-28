5c0334cedd2f9db9de840194d78bde13acd8ef5f
motomasa.suzuki <m_suzuki@suzukitsuyoshirinoMacBook-Pro.local>
2025-01-28 14:29:42 +0900

    poetry_install.sh の修正

--- poetry_install.sh
+++ poetry_install.sh
@@ -2,26 +2,10 @@
 
 curl -sSL https://install.python-poetry.org | python3 -
 
-CURRENT_SHELL=$(basename "$SHELL")
-
-# 追加するPATH設定
-ADD_PATH='export PATH="$HOME/.local/bin:$PATH"'
-
-# 設定ファイルのパスを決定
-if [ "$current_shell" = "bash" ]; then
-    config_file="$HOME/.bashrc"
-elif [ "$current_shell" = "zsh" ]; then
-    config_file="$HOME/.zshrc"
+if grep -Fxq "$EXPORT_LINE" "$TARGET_RC"; then
+    echo "$TARGET_RC に既に追記済みです。"
 else
-    echo "現在使用しているシェルはbashでもzshでもありません。手動でPATHを追加してください。"
-    exit 1
-fi
-
-# PATH設定が既に存在するか確認
-if grep -Fxq "$path_export" "$config_file"; then
-    echo "既に$path_exportが$config_fileに追加されています。"
-else
-    # PATH設定を設定ファイルに追加
-    echo "$path_export" >>"$config_file"
-    echo "$path_export を $config_file に追加しました。"
+    EXPORT_LINE="export PATH=\"\$HOME/.local/share/pypoetry/venv/bin:\$PATH\""
+    TARGET_RC="$HOME/.bashrc"
+    echo "$EXPORT_LINE" >>"$TARGET_RC"
 fi
