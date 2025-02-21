161bf8d2ae5d2703facc9989e2b1a76b32c8f90b
moto <moto0123moto@gmail.com>
2025-02-21 12:18:45 +0900

    test ツール周りの整備

--- test_dotfiles/entrypoint.sh
+++ test_dotfiles/entrypoint.sh
@@ -3,28 +3,18 @@ set -e
 
 # クローン先ディレクトリ
 DOTFILES_DIR="$HOME/dotfiles"
+BRANCH_NAME="update-script"
+COMMIT_HASH=43e1805b78cf14d755b3584a6ee2b7e62ea41279
 
-# dotfiles リポジトリの URL（適宜ご自身のリポジトリに変更してください）
 REPO_URL="https://github.com/lMotol/dotfiles.git"
 
 # リポジトリが既に存在していなければクローン
 if [ ! -d "$DOTFILES_DIR/.git" ]; then
     echo "Cloning dotfiles repository..."
-    git clone "$REPO_URL" "$DOTFILES_DIR"
+    git clone "$REPO_URL" "$DOTFILES_DIR" -b $BRANCH_NAME
 else
     echo "Dotfiles repository already exists. Pulling latest changes..."
     cd "$DOTFILES_DIR" && git pull
 fi
 
 cd $DOTFILES_DIR
-
-# dotfiles 内にセットアップスクリプト（例: setup.sh）がある場合は実行
-if [ -f "install.sh" ]; then
-    echo "Running setup script..."
-    bash "install.sh"
-else
-    echo "No setup script found in the dotfiles repository."
-fi
-
-# 必要に応じてシェルを起動（または、他のコマンドを実行）
-exec bash
